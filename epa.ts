d133ee9dc68f8c95658e564648fa7deeda4e6ab0
codeagent <i.aleksey.yakovlev@gmail.com>
2021-05-23 22:29:36 +0200

    control point

--- epa.ts
+++ epa.ts
@@ -1,15 +1,23 @@
+import value from '*.json';
 import { vec2, vec3 } from 'gl-matrix';
 import { gjk } from './gjk';
 import { PriorityQueue } from './priority-queue';
 
 export namespace epa {
-  interface Face<T> {
-    vertices: [T, T, T];
-    siblings: [Face<T>, Face<T>, Face<T>];
+  interface Loop<T> {
+    v: T;
+    next: Loop<T>;
+  }
+
+  interface Face<T extends object> {
+    // vertices: Loop<T>; // v0 -> v1 -> v2 -> v0
+    vertices: T[];
+    loop: WeakMap<T, T>; // v(i) -> v(i+1)
+    siblings: WeakMap<T, Face<T>>; // v0 -> face
     distance: number;
   }
 
-  type Polytop<T = vec3> = PriorityQueue<Face<T>>;
+  type Polytop<T extends object = vec3> = PriorityQueue<Face<T>>;
 
   const calcFaceDistance = (face: Face<vec3>): number => {
     const O = vec3.create();
@@ -22,13 +30,26 @@ export namespace epa {
     return Math.abs(vec3.dot(O, x)) / length;
   };
 
+  const makeLoop = <T extends object>(values: T[]): WeakMap<T, T> => {
+    const loop = new WeakMap<T, T>();
+    for (let i = 0; i < values.length; i++) {
+      loop.set(values[i], values[(i + 1) % values.length]);
+    }
+    return loop;
+  };
+
   export const polytopFromSimplex = (simplex: gjk.Simplex<vec3>): Polytop => {
     const vertices = Array.from(simplex);
 
+    const w0 = vertices[0];
+    const w1 = vertices[1];
+    const w2 = vertices[2];
+    const w3 = vertices[2];
     const w1w0 = vec3.create();
     const w2w0 = vec3.create();
-    vec3.subtract(w1w0, vertices[1], vertices[0]);
-    vec3.subtract(w2w0, vertices[2], vertices[0]);
+
+    vec3.subtract(w1w0, w1, w0);
+    vec3.subtract(w2w0, w2, w0);
 
     const x = vec3.create();
     vec3.cross(x, w2w0, w1w0);
@@ -45,33 +66,53 @@ export namespace epa {
     }
 
     const face0: Face<vec3> = {
-      vertices: [vertices[0], vertices[1], vertices[3]],
+      vertices: [w0, w1, w3],
+      loop: makeLoop([w0, w1, w3]),
       siblings: null,
       distance: 0.0
     };
 
     const face1: Face<vec3> = {
-      vertices: [vertices[1], vertices[2], vertices[3]],
+      vertices: [w1, w2, w3],
+      loop: makeLoop([w1, w2, w3]),
       siblings: null,
       distance: 0.0
     };
 
     const face2: Face<vec3> = {
-      vertices: [vertices[2], vertices[0], vertices[3]],
+      vertices: [w2, w0, w3],
+      loop: makeLoop([w2, w0, w3]),
       siblings: null,
       distance: 0.0
     };
 
     const face3: Face<vec3> = {
-      vertices: [vertices[1], vertices[0], vertices[2]],
+      vertices: [w1, w0, w2],
+      loop: makeLoop([w1, w0, w2]),
       siblings: null,
       distance: 0.0
     };
 
-    face0.siblings = [face3, face1, face2];
-    face1.siblings = [face3, face2, face0];
-    face2.siblings = [face3, face0, face1];
-    face3.siblings = [face0, face2, face1];
+    face0.siblings = new WeakMap<vec3, Face<vec3>>([
+      [w0, face3],
+      [w1, face1],
+      [w3, face2]
+    ]);
+    face1.siblings = new WeakMap<vec3, Face<vec3>>([
+      [w1, face3],
+      [w2, face2],
+      [w3, face0]
+    ]);
+    face2.siblings = new WeakMap<vec3, Face<vec3>>([
+      [w2, face3],
+      [w0, face0],
+      [w3, face1]
+    ]);
+    face3.siblings = new WeakMap<vec3, Face<vec3>>([
+      [w1, face0],
+      [w0, face2],
+      [w2, face1]
+    ]);
 
     const queue = new PriorityQueue<Face<vec3>>(
       (a: Face<vec3>, b: Face<vec3>) => a.distance - b.distance
@@ -98,11 +139,7 @@ export namespace epa {
     gjk.closestPointToTriangle(b3, w0, w1, w2, O);
 
     const w4 = vec3.create();
-    gjk.fromBarycentric(
-      w4,
-      [face.vertices[0], face.vertices[1], face.vertices[2]],
-      b3
-    );
+    gjk.fromBarycentric(w4, [w0, w1, w2], b3);
     shape.support(w4, w4);
 
     // w0-w1
@@ -127,116 +164,112 @@ export namespace epa {
     // split current face
     const f0 = {
       vertices: [w0, w4, w2h],
+      loop: makeLoop([w0, w4, w2h]),
       siblings: null,
       distance: 0.0
     };
     const f1 = {
       vertices: [w0, w0h, w4],
+      loop: makeLoop([w0, w0h, w4]),
       siblings: null,
       distance: 0.0
     };
 
     const f2 = {
       vertices: [w1, w4, w0h],
+      loop: makeLoop([w1, w4, w0h]),
       siblings: null,
       distance: 0.0
     };
     const f3 = {
       vertices: [w1, w1h, w4],
+      loop: makeLoop([w1, w1h, w4]),
       siblings: null,
       distance: 0.0
     };
 
     const f4 = {
       vertices: [w2, w4, w1h],
+      loop: makeLoop([w2, w4, w1h]),
       siblings: null,
       distance: 0.0
     };
     const f5 = {
       vertices: [w2, w2h, w4],
+      loop: makeLoop([w2, w2h, w4]),
       siblings: null,
       distance: 0.0
     };
 
     // split sibling0
-    const ws0Index =
-      face.siblings[0].vertices[0] !== w0 && face.siblings[0].vertices[0] !== w1
-        ? 0
-        : face.siblings[0].vertices[1] !== w0 &&
-          face.siblings[0].vertices[1] !== w1
-        ? 1
-        : 2;
-
-    const ws0 = face.siblings[0].vertices[ws0Index];
-    const s0s = face.siblings[(ws0Index + 2) % 3];
-    const s1s = face.siblings[ws0Index];
-
-    const s0 = {
+    const s0 = face.siblings.get(w0);
+    const ws0 = s0.loop.get(w0);
+    const sf0 = {
       vertices: [ws0, w0h, w0],
+      loop: makeLoop([ws0, w0h, w0]),
       siblings: null,
       distance: 0.0
     };
-    const s1 = {
+    const sf1 = {
       vertices: [ws0, w1, w0h],
+      loop: makeLoop([ws0, w1, w0h]),
       siblings: null,
       distance: 0.0
     };
 
     // split sibling1
-    const ws1Index =
-      face.siblings[1].vertices[0] !== w0 && face.siblings[1].vertices[0] !== w1
-        ? 0
-        : face.siblings[1].vertices[1] !== w0 &&
-          face.siblings[1].vertices[1] !== w1
-        ? 1
-        : 2;
-
-    const ws1 = face.siblings[1].vertices[ws1Index];
-    const s2s = face.siblings[(ws1Index + 2) % 3];
-    const s3s = face.siblings[ws1Index];
-    const s2 = {
+    const s1 = face.siblings.get(w1);
+    const ws1 = s1.loop.get(w1);
+    const sf2 = {
       vertices: [ws1, w1h, w1],
+      loop: makeLoop([ws1, w1h, w1]),
       siblings: null,
       distance: 0.0
     };
-    const s3 = {
+    const sf3 = {
       vertices: [ws1, w2, w1h],
+      loop: makeLoop([ws1, w2, w1h]),
       siblings: null,
       distance: 0.0
     };
 
     // split sibling2
-    const ws2Index =
-      face.siblings[2].vertices[0] !== w0 && face.siblings[2].vertices[0] !== w1
-        ? 0
-        : face.siblings[2].vertices[1] !== w0 &&
-          face.siblings[2].vertices[1] !== w1
-        ? 1
-        : 2;
-    const ws2 = face.siblings[2].vertices[ws2Index];
-    const s4s = face.siblings[(ws2Index + 2) % 3];
-    const s5s = face.siblings[ws2Index];
-    const s4 = {
+    const s2 = face.siblings.get(w2);
+    const ws2 = s2.loop.get(w2);
+    const sf4 = {
       vertices: [ws2, w2h, w2],
+      loop: makeLoop([ws2, w2h, w2]),
       siblings: null,
       distance: 0.0
     };
-    const s5 = {
+    const sf5 = {
       vertices: [ws2, w0, w2h],
+      loop: makeLoop([ws2, w0, w2h]),
       siblings: null,
       distance: 0.0
     };
 
     // assignning to neighbours
-    f0.siblings = [f1, f5, s5];
-    f1.siblings = [s0, f2, f0];
-    f2.siblings = [f3, f1, s1];
-    f3.siblings = [s2, f4, f2];
-    f4.siblings = [f5, f3, s3];
-    f5.siblings = [s4, f0, f4];
+    f0.siblings = new WeakMap([[w0, f1], [w4, f5], [w2h, sf5]]);
+    f1.siblings = new WeakMap([[w0, sf0], [w0h, f2], [w4, f0]]);
+    f2.siblings = new WeakMap([[w1, f3], [w4, f1], [w0h, sf1]]);
+    f3.siblings = new WeakMap([[w1, sf2], [w1h, f4], [w4, f2]]);
+    f4.siblings = new WeakMap([[w2, f5], [w4, f3], [w1h, sf3]]);
+    f5.siblings = new WeakMap([[w2, sf4], [w2h, f0], [w4, f4]]);
 
-    // s0.siblings = [s1, f1, s0s];
-    // s0s
+    // @todo:
+    //sf0.siblings = new WeakMap([[w0, f1], [w4, f5], [w2h, sf5]]);
+
+    // remove siblings
+    polytop.remove(s0);
+    polytop.remove(s1);
+    polytop.remove(s2);
+
+    // insert newly generated faces
+    for (let face of [f0, f1, f2, f3, f4, f5, sf0, sf1, sf2, sf3, sf4, sf5]) {
+      calcFaceDistance(face);
+      polytop.enqueue(face);
+    }
   };
 
   export const contactNormal = (
